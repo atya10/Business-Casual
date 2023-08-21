766aa23543cf041ce2b47c6cd3872d5751f52ab3
Atya M. Jarghoun <atya.tee37@gmail.com>
2023-08-21 14:33:39 +0300

    new-edit

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[MainController::class,'index'])->name('index');
-Route::get('/about',[MainController::class,'about'])->name('about');
-Route::get('/products',[MainController::class,'products'])->name('products');
-Route::get('/store',[MainController::class,'store'])->name('store');
+Route::get('/',[MainController::class,'index']) ->name('index');
+Route::get('/about',[MainController::class,'about']) ->name('about');
+Route::get('/products',[MainController::class,'products']) ->name('products');
+Route::get('/store',[MainController::class,'store']) ->name('store');
+
+
+
 
 
 //
+
